9d5c40dccdb1dd2e12840c85a0ff803b94b8cd46
Phong <tranxuanhoat2003@gmail.com>
2024-09-24 11:13:36 +0700

    fix_pag part 2

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -12,20 +12,28 @@
     <meta name="og:keywords" content="Shinex – nhãn hàng dược mỹ phẩm hàng đầu mang đến giải pháp chuyên sâu cho mọi loại da, kể cả làn da nhạy cảm nhất.">
 @endsection
 @section('content')
-    <div class="grid md:grid-cols-2 mt-[56px]">
-        <div class="col-span-1 h-full group/item relative">
-            <img class="h-full " src="{{asset('images/components/adsHome_2.jpg')}}" alt="">
-            <div class="absolute text-center bottom-0 text-white w-full h-full bg-black bg-opacity-40 flex flex-col justify-center">
-                <h3 class="text-2xl">NGUỒN NGUYÊN LIỆU QUÝ HIẾM</h3>
-                <p class="underline group-hover/item:block hidden duration-500 ease-in-out ">Từ thiên nhiên Hàn Quốc</p>
+    <div class="bg-gray-100 flex items-center justify-center h-screen">
+        <div class="flex w-full h-[80%] transition-all duration-500">
+            <div class="relative w-[50%] h-full overflow-hidden transition-all duration-700 hover:w-[60%] group/item">
+                <a href="#">
+                    <img src="{{asset('images/components/adsHome_2.jpg')}}" alt="Image 1"
+                         class="h-full w-full object-cover transition-all duration-700">
+                    <div class="absolute text-center bottom-0 text-white w-full h-full bg-black bg-opacity-40 flex flex-col justify-center">
+                        <h3 class="text-2xl">NGUỒN NGUYÊN LIỆU QUÝ HIẾM</h3>
+                        <p class="underline group-hover/item:block hidden duration-700 transition-all ease-in-out">Từ thiên nhiên Hàn Quốc</p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="relative w-[50%] h-full overflow-hidden transition-all duration-700 hover:w-[60%] group/item">
+                <a href="#">
+                    <img src="{{asset('images/components/adsHome_1.jpg')}}" alt="Image 2"
+                         class="h-full w-full object-cover transition-all duration-700">
+                    <div class="absolute text-center bottom-0 text-white w-full h-full bg-black bg-opacity-40 flex flex-col justify-center">
+                        <h3 class="text-2xl">CÔNG NGHỆ LÊN MEN KÉP</h3>
+                        <p class="underline group-hover/item:block hidden duration-700 transition-all ease-in-out">Độc quyền hiện đại</p>
+                    </div>
+                </a>
             </div>
         </div>
-        <div class="col-span-1 h-full group/item relative">
-            <img class="h-full " src="{{asset('images/components/adsHome_1.jpg')}}" alt="">
-            <div class="absolute text-center bottom-0 text-white w-full h-full bg-black bg-opacity-40 flex flex-col justify-center">
-                <h3 class="text-2xl">CÔNG NGHỆ LÊN MEN KÉP</h3>
-                <p class="underline group-hover/item:block hidden duration-500 ease-in-out ">Độc quyền hiện đại</p>
-            </div>
-        </div>
-    </div>
 @endsection
